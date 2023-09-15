abcc4c79cbd31bed707371021058f407ba37cfe0
harshkanani014 <harshkanani100@gmail.com>
2023-09-15 16:48:41 +0530

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 
-const PORajkdajkdladn = 5001
+const PORajkdajkdladn = 5005
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
